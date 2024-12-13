c17b18f82d321c3da099237f03f8dfa8d31f1635
Sulthan-Sulthan <sulthanulparis@gmail.com>
2024-12-13 16:27:45 +0530

    some changes

--- Python/demo.py
+++ Python/demo.py
@@ -1,44 +1,211 @@
 import psycopg2
-import json
+from camelcase import CamelCase
+c = CamelCase()
 
 
-try:
-    conn = psycopg2.connect(database="postgres",
-                            host="localhost",
-                            user="postgres",
-                            password="postgreDB@900",
-                            port="5432")
-    cursor = conn.cursor()
 
-    CREATE_TABLE_QUERY = """
-    CREATE TABLE IF NOT EXISTS USER_DATA (
-        T_userId SERIAL PRIMARY KEY,            -- Auto-increment primary key
-        Name VARCHAR(200),                      -- Name with a maximum size of 200 characters
-        first_Name VARCHAR(100) NOT NULL,       -- First name, not null
-        last_Name VARCHAR(100) NOT NULL,        -- Last name, not null
-        email VARCHAR(255) NOT NULL UNIQUE,     -- Email, must be unique and not null
-        dateOfBirth DATE,                       -- Date of birth in DATE format
-        createdOwner TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Automatically set to current time
-        createdBy VARCHAR(100) NOT NULL,        -- Name of the creator, not null
-        modified TIMESTAMP                      -- Nullable column for last modification time
-    );
-    """
-    cursor.execute(CREATE_TABLE_QUERY)
-    conn.commit()
-    print("")
-    # print()
-    # cursor.execute("SELECT * FROM T_user ")
-    # # cursor.execute()
+class Sql_operation:
+    def __init__(self):
+         self.conn = psycopg2.connect(database="postgres",
+                        host="localhost",
+                        user="postgres",
+                        password="postgreDB@900",
+                        port="5432")
+        
+    def creating_table(self):
+        
+        cursor = self.conn.cursor()
 
-    # columns = [col[0] for col in cursor.description]
-    # print([dict(zip(columns, row)) for row in cursor.fetchall()])
-    #a = cursor.fetchall()
-    #print(a)
-    # # json_data = [json.loads(row[0]) for row in a]
+        cursor.execute('drop table if exists new_table ')
+        creating_table= '''CREATE TABLE IF NOT EXISTS NEW_TABLE(
+        ID INT PRIMARY KEY,
+        NAME VARCHAR(20),
+        SALARY INT ,
+        DEPT_NAME VARCHAR(20)
+        )
+        '''
+        cursor.execute(creating_table)
+        self.conn.commit()
 
-    # # print(json_data)
-    # # print(cursor.fetchall())
-    # # key_value_dict = {row[0]: row[1] for row in a}
-    # # print(key_value_dict)
-except Exception as err:
-    print(err)
+
+    def inserting_values(self):
+        cursor = self.conn.cursor()
+        namedict = ({ "ID":1,"NAME":"Joshua", "SALARY":2000 ,"DEPT_NAME":"DP1"},
+                    {"ID":2, "NAME":"Steven", "SALARY":4000 ,"DEPT_NAME":"DP2"},
+                    {"ID":3, "NAME":"David", "SALARY":6000 ,"DEPT_NAME":"DP3"})
+    
+        cursor.executemany("""INSERT INTO NEW_TABLE(ID, NAME,SALARY,DEPT_NAME) VALUES (%(ID)s, %(NAME)s , %(SALARY)s , %(DEPT_NAME)s)""", namedict)
+        self.conn.commit()
+
+    def update(self,name):
+        c.hump(name)
+        cursor = self.conn.cursor()
+        id_dict = { "ID":2}
+        query = "UPDATE new_table SET name = %(name)s WHERE id = %(ID)s"
+        cursor.execute(query, id_dict)
+        self.conn.commit()
+
+    
+demo =Sql_operation()
+demo.creating_table()
+demo.inserting_values()
+demo.update("faris")
+
+
+
+
+
+
+
+
+# class sql_operation:
+
+#     def create():
+
+
+# cursor.execute('drop table if exists new_table ')
+# creating_table= '''CREATE TABLE IF NOT EXISTS NEW_TABLE(
+# ID INT PRIMARY KEY,
+# NAME VARCHAR(20),
+# SALARY INT ,
+# DEPT_NAME VARCHAR(20)
+# )
+# '''
+# cursor.execute(creating_table)
+
+
+# def insert():  
+
+    
+
+
+
+
+ 
+
+    
+    # cursor.execute(f''' UPDATE new_table
+    #                 SET NAME ='{name}'
+    #                 WHERE id = 1''')
+
+    # query = "UPDATE {table_name} SET {data} WHERE id = {id};".format(table_name=table_name, data=data, id=id)
+    
+    # id_dict = { "ID":2}
+    # query = "UPDATE new_table SET name = 'faris' WHERE id = %(ID)s"
+    # cursor.execute(query, id_dict)
+
+
+    
+    # print(cursor.fetchone())
+    # update_salary = 'update NEW_TABLE set salary = salary + (salary * 0.1)'
+    # cursor.execute(update_salary)
+
+
+
+# def delete():
+#     delete_row = 'delete from new_table where name = %(id)s '
+#     delete_name = ('faris',)
+#     cursor.execute(delete_row,delete_name)
+
+# insert()
+# update("faris")
+
+# conn.commit()
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print()
+# cursor.execute("SELECT * FROM T_user ")
+# # cursor.execute()
+
+
+
+
+
+# columns = [col[0] for col in cursor.description]
+# print([dict(zip(columns, row)) for row in cursor.fetchall()])
+
+
+
+
+
+#a = cursor.fetchall()
+#print(a)
+# # json_data = [json.loads(row[0]) for row in a]
+
+# # print(json_data)
+# # print(cursor.fetchall())
+# # key_value_dict = {row[0]: row[1] for row in a}
+# # print(key_value_dict)
